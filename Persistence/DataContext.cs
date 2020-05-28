4457a6cf87d0b3fbb0fb3b8239a2805db29163cd
Manh <nguyenducmanhmmo@gmail.com>
2020-05-28 23:36:45 +0700

    Init project

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions option) : base(option)
+        {
+            
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+
+}
